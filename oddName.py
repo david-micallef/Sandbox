6c462af235e93bde7a1ebb8aba3a34e645d44f66
david-micallef <david.micallef@my.jcu.edu.au>
2017-08-07 13:29:35 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,10 @@
 """ David """
+
+
+username = str(input("Enter username: "))
+while username is "":
+    print("No username entered.")
+    username = str(input("Enter username: "))
+
+for i in range(0,len(username),2):
+    print(username[i], end= "")
